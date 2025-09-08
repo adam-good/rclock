d789bb3538d206bd49209a17afa8e3d3128ba0b9
Adam Good <dev.adamgood@gmail.com>
2025-09-08 14:49:14 -0400

    made sample more of a clock

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
+use std::time::Duration;
+
 use color_eyre::Result;
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
-    DefaultTerminal,
-    Frame,
-    style::Stylize,
-    prelude::Rect,
+    DefaultTerminal, Frame,
     buffer::Buffer,
+    prelude::Rect,
+    style::Stylize,
+    symbols::border,
     text::{Line, Text},
-    widgets::{Block, Widget, Paragraph},
-    symbols::border
+    widgets::{Block, Paragraph, Widget},
 };
-use chrono::DateTime
 
-#[derive(Debug)]
+use chrono::{DateTime, Local};
+
+#[derive(Debug, Default)]
 struct App {
-    time: SystemTime,
-    exit: bool
+    time: DateTime<Local>,
+    exit: bool,
 }
 
 impl App {
     fn run(&mut self, mut term: DefaultTerminal) -> Result<()> {
         while !self.exit {
+            self.time = Local::now();
             term.draw(|frame| self.render(frame))?;
             self.handle_events()?;
         }
@@ -33,41 +36,39 @@ impl App {
     }
 
     fn handle_events(&mut self) -> Result<()> {
+        if !event::poll(Duration::from_secs(1))? {
+            return Ok(());
+        }
         match event::read()? {
             // it's important to check that the event is a key press event as
             // crossterm also emits key release and repeat events on Windows.
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                match key_event.code {
-                    KeyCode::Char('q') => self.exit = true,
-                    _ => {}
-                }
+                self.handle_key_event(key_event);
             }
             _ => {}
         };
         Ok(())
     }
+
+    fn handle_key_event(&mut self, key_event: KeyEvent) {
+        match key_event.code {
+            KeyCode::Char('q') => self.exit = true,
+            _ => {}
+        }
+    }
 }
 
 impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer)
-    {
+    fn render(self, area: Rect, buf: &mut Buffer) {
         let title = Line::from(" RClock ".bold());
-        let instructions = Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]);
+        let instructions = Line::from(vec![" Quit ".into(), "<Q> ".blue().bold()]);
         let block = Block::bordered()
             .title(title.centered())
             .title_bottom(instructions.centered())
             .border_set(border::THICK);
 
         let counter_text = Text::from(vec![Line::from(vec![
-            "Value: ".into(),
-            self.time.to_string().yellow(),
+            self.time.format("%H:%M:%S").to_string().yellow(),
         ])]);
 
         Paragraph::new(counter_text)
@@ -82,7 +83,6 @@ fn main() -> Result<()> {
     let terminal = ratatui::init();
     let result = App::default().run(terminal);
     ratatui::restore();
+
     result
 }
-
-
