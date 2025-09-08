a51dda09532d71a67b33abece797c1dfdc27bf07
Adam Good <dev.adamgood@gmail.com>
2025-09-07 21:20:19 -0400

    changing to ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
+use color_eyre::Result;
+use crossterm::event::{self, Event};
+use ratatui::{DefaultTerminal, Frame};
 
-
-
-fn main() {
-    println!("Hello, world!");
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello w0rld", frame.area());
 }
+
+fn run(mut term: DefaultTerminal) -> Result<()> {
+    loop {
+        term.draw(render)?;
+        if matches!(event::read()?, Event::Key(_)) {
+            break Ok(());
+        }
+    }
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+    result
+}
+
+
