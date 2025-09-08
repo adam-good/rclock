d853c7f70fcc98f906c8ae6ec38179af1f6ea4f7
Adam Good <dev.adamgood@gmail.com>
2025-09-08 10:32:45 -0400

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,86 @@
 use color_eyre::Result;
-use crossterm::event::{self, Event};
-use ratatui::{DefaultTerminal, Frame};
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    DefaultTerminal,
+    Frame,
+    style::Stylize,
+    prelude::Rect,
+    buffer::Buffer,
+    text::{Line, Text},
+    widgets::{Block, Widget, Paragraph},
+    symbols::border
+};
+use chrono::DateTime
 
-fn render(frame: &mut Frame) {
-    frame.render_widget("hello w0rld", frame.area());
+#[derive(Debug)]
+struct App {
+    time: SystemTime,
+    exit: bool
 }
 
-fn run(mut term: DefaultTerminal) -> Result<()> {
-    loop {
-        term.draw(render)?;
-        if matches!(event::read()?, Event::Key(_)) {
-            break Ok(());
+impl App {
+    fn run(&mut self, mut term: DefaultTerminal) -> Result<()> {
+        while !self.exit {
+            term.draw(|frame| self.render(frame))?;
+            self.handle_events()?;
         }
+
+        Ok(())
+    }
+
+    fn render(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> Result<()> {
+        match event::read()? {
+            // it's important to check that the event is a key press event as
+            // crossterm also emits key release and repeat events on Windows.
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                match key_event.code {
+                    KeyCode::Char('q') => self.exit = true,
+                    _ => {}
+                }
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer)
+    {
+        let title = Line::from(" RClock ".bold());
+        let instructions = Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q> ".blue().bold(),
+        ]);
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
+
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Value: ".into(),
+            self.time.to_string().yellow(),
+        ])]);
+
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
     }
 }
 
 fn main() -> Result<()> {
     color_eyre::install()?;
     let terminal = ratatui::init();
-    let result = run(terminal);
+    let result = App::default().run(terminal);
     ratatui::restore();
     result
 }
