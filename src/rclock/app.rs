3b2d8e065ffe6ad4a0c73f2ad5b682e5a8f1a6c4
Adam Good <dev.adamgood@gmail.com>
2025-09-15 18:25:19 -0400

    added stop functionality to app

--- src/rclock/app.rs
+++ src/rclock/app.rs
@@ -3,6 +3,7 @@ use chrono::Local;
 use std::io;
 
 use std::fmt;
+use std::io::Error;
 
 use crate::rclock::timer;
 
@@ -10,6 +11,13 @@ pub struct App {
     pub base_time: DateTime<Local>,
     primary_timer_idx: Option<usize>,
     timers: Vec<timer::Timer>,
+    state: AppState,
+}
+
+#[derive(Eq, PartialEq)]
+enum AppState {
+    Running,
+    Stopped,
 }
 
 impl App {
@@ -18,7 +26,20 @@ impl App {
             base_time: Local::now(),
             primary_timer_idx: None,
             timers: Vec::<timer::Timer>::new(),
+            state: AppState::Stopped,
         }
+    }
+
+    pub fn run(&mut self) {
+        self.state = AppState::Running;
+    }
+
+    pub fn stop(&mut self) {
+        self.state = AppState::Stopped;
+    }
+
+    pub fn is_running(&mut self) -> bool {
+        self.state == AppState::Running
     }
 
     pub fn new_timer(&mut self) -> io::Result<()> {
@@ -27,6 +48,11 @@ impl App {
     }
 
     pub fn update(&mut self) -> io::Result<()> {
+        if self.state == AppState::Stopped {
+            let err = Error::new(io::ErrorKind::Other, "App Not Running");
+            return Err(err);
+        }
+
         self.base_time = Local::now();
         for t in &mut self.timers {
             t.update();
