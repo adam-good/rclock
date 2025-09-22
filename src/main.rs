289d70ca32203444c72a201215c634d8f4ae7d5c
Adam Good <dev.adamgood@gmail.com>
2025-09-22 17:29:02 -0400

    removed sleep

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
-use std::time::Duration;
-
 mod rclock;
 use rclock::app;
 use rclock::ui;
-
-fn sleep(n: u64) {
-    std::thread::sleep(Duration::new(n, 0));
-}
 
 fn main() {
     let mut app: app::App = app::App::new();
@@ -14,14 +8,10 @@ fn main() {
 
     app.new_pomodoro(3, 1, 2);
     app.run();
-
-    let mut i = 0;
-    while i < 60 * 10 && app.is_running() {
+    while app.is_running() {
         app.update().unwrap();
         app_ui.view(&app);
         app_ui.handle_events(&mut app).expect("UH OH");
-        sleep(1);
-        i = i + 1;
     }
 
     app_ui.shutdown(&mut app);
