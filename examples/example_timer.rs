c6739e5bce63d98190c47984fbb36e5bf1c27913
Adam Good <dev.adamgood@gmail.com>
2026-08-13 20:59:10 -0400

    example: timer example

--- examples/example_timer.rs
+++ examples/example_timer.rs
@@ -1,11 +1,20 @@
 
 
-use rclock;
+use std::time::Duration;
+use std::thread::sleep;
+
+use rclock::utils::timer::Timer;
 
 fn main() {
-    let timer = rclock::utils::Timer::new(std::time::Instant::now());
-
-    std::thread::sleep(std::time::Duration::new(2, 0));
-
-    println!("{:?}", timer.tick());
+    let timer = Timer::from_now().run();
+    let sleep_time = Duration::new(1, 0); 
+    loop {
+        let timer = timer.tick();
+        print!("{}\n", timer);
+        sleep(sleep_time);
+       
+        if timer.duration().as_secs() > 5 {
+            break;
+        }
+    }
 }
