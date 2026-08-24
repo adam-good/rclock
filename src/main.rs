af3201769e81907c8fe5c9284eacdacf17237fe6
Adam Good <adamgood@protonmail.com>
2026-08-24 13:07:01 -0400

    wip: strip main.rs for testing

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,40 @@
-use clap::{Arg, Command};
-
-mod config;
-mod rclock;
-use config::Config;
-use rclock::{app, ui};
+//use clap::{Arg, Command};
+//
+//mod config;
+//mod rclock;
+//use config::Config;
+//use rclock::{app, ui};
 
 fn main() {
-    let matches = Command::new("rclock")
-        .args(vec![
-            Arg::new("rounds")
-                .short('r')
-                .long("rounds")
-                .help("How Many Rounds in One Cycle"),
-            Arg::new("work")
-                .short('w')
-                .long("work")
-                .help("How Many Minutes on the Work Timer"),
-            Arg::new("break")
-                .short('b')
-                .long("break")
-                .help("How Many Minutes on the Break Timer"),
-        ])
-        .get_matches();
-
-    let config: Config = Config::default().from_config(None).from_matches(matches);
-
-    let mut app: app::App = app::App::new(config);
-    let mut app_ui: ui::UI = ui::UI::new();
-
-    app.new_pomodoro();
-    app.run();
-    while app.is_running() {
-        app.update().unwrap();
-        app_ui.view(&app);
-        app_ui.handle_events(&mut app).expect("UH OH");
-    }
-
-    app_ui.shutdown(&mut app);
+//    let matches = Command::new("rclock")
+//        .args(vec![
+//            Arg::new("rounds")
+//                .short('r')
+//                .long("rounds")
+//                .help("How Many Rounds in One Cycle"),
+//            Arg::new("work")
+//                .short('w')
+//                .long("work")
+//                .help("How Many Minutes on the Work Timer"),
+//            Arg::new("break")
+//                .short('b')
+//                .long("break")
+//                .help("How Many Minutes on the Break Timer"),
+//        ])
+//        .get_matches();
+//
+//    let config: Config = Config::default().from_config(None).from_matches(matches);
+//
+//    let mut app: app::App = app::App::new(config);
+//    let mut app_ui: ui::UI = ui::UI::new();
+//
+//    app.new_pomodoro();
+//    app.run();
+//    while app.is_running() {
+//        app.update().unwrap();
+//        app_ui.view(&app);
+//        app_ui.handle_events(&mut app).expect("UH OH");
+//    }
+//
+//    app_ui.shutdown(&mut app);
 }
